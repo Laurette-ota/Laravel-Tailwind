4aa425546eae8d81bf862df22210c8d6bace44f3
Laurette <borisiakokouvi@gmail.com>
2025-02-11 17:39:51 +0100

    Ajout de fichiers de configuration, de styles et de tests pour initialiser le projet

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  @vite('resources/css/app.css')
+</head>
+
+<body>
+  <h1 class="bg-purple-600 hover:bg-green-600 bg-opacity-10 backdrop-blur-md brightness-110 p-8 border border-white border-opacity-20 rounded-lg font-bold text-3xl underline filter text:center">
+    Hello world!
+  </h1>
+
+</body>
+
+</html>

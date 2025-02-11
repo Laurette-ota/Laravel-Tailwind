6090c9096bffa2fee05df25825fda441ff44afc6
Laurette <borisiakokouvi@gmail.com>
2025-02-11 18:09:06 +0100

    Correction de la balise viewport et mise à jour des styles dans les fichiers app.blade.php et home.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,12 +3,12 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="viewport" content="wi dth=device-width, initial-scale=1.0" />
   @vite('resources/css/app.css')
 </head>
 
 <body>
-  <h1 class="bg-purple-600 hover:bg-green-600 bg-opacity-10 backdrop-blur-md brightness-110 p-8 border border-white border-opacity-20 rounded-lg font-bold text-3xl underline filter text:center">
+  <h1 class="bg-purple-600 hover:bg-green-600">
     Hello world!
   </h1>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,10 @@
     <link rel="icon" type="image/svg+xml" href="favicon.svg">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homesmart</title>
-    @vite('resources/css/app.css')
+    <!-- @vite('resources/css/app.css') -->
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
-<body class="bg-blue-500">
-    
+<body class="bg-amber-300 mx-auto max-w-[2000px] text-neutral-900">
+    <h1> Hey BOBO</h1>
 </body>
 </html>
